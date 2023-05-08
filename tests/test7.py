82eed7fe4f4f44e2e8f7129f1a0a82653f87c6f9
Terry Greeniaus <tgreeniaus@gmail.com>
2023-05-07 18:50:21 -0600

    update test7 to test changing plot bounds

--- tests/test7.py
+++ tests/test7.py
@@ -1,4 +1,5 @@
 import math
+from enum import Enum
 
 import numpy as np
 
@@ -8,7 +9,8 @@ import glotlib
 NVERTICES = 500000
 Xi        = np.arange(NVERTICES) * 2 * math.pi / (NVERTICES - 1)
 
-BOUNDS = [311, 312, 313]
+H_BOUNDS = [311, 312, 313]
+V_BOUNDS = [131, 132, 133]
 COLORS = [
     (0, 0.8, 0.2, 1),
     (0.8, 0.2, 0, 1),
@@ -27,13 +29,19 @@ THICK_RATES = [
 Xs = [ar * Xi for ar in AMP_RATES]
 
 
+class ViewMode(Enum):
+    HORIZONTAL  = 1
+    VERTICAL    = 2
+
+
 class Window(glotlib.Window):
     def __init__(self):
         super().__init__(900, 650, msaa=4)
 
-        self.series = []
+        self.series    = []
+        self.view_mode = ViewMode.HORIZONTAL
 
-        for bounds, color, X in zip(BOUNDS, COLORS, Xs):
+        for bounds, color, X in zip(H_BOUNDS, COLORS, Xs):
             p = self.add_plot(bounds, limits=(0, -1, 2 * math.pi, 1))
 
             Y = np.sin(X)
@@ -41,6 +49,30 @@ class Window(glotlib.Window):
 
         self.add_label((.1, 12.1 / 13), 'tcdc_6_2000014_n4.csv')
         self.fps_label = self.add_label((0.01, 0.01), '')
+
+    def _set_mode_vertical(self):
+        if self.view_mode == ViewMode.VERTICAL:
+            return
+
+        for bounds, p in zip(V_BOUNDS, self.plots):
+            p.set_bounds(bounds)
+        self.view_mode = ViewMode.VERTICAL
+
+    def _set_mode_horizontal(self):
+        if self.view_mode == ViewMode.HORIZONTAL:
+            return
+
+        for bounds, p in zip(H_BOUNDS, self.plots):
+            p.set_bounds(bounds)
+        self.view_mode = ViewMode.HORIZONTAL
+
+    def handle_key_press(self, key):
+        if key == glotlib.KEY_ESCAPE:
+            if self.view_mode == ViewMode.HORIZONTAL:
+                self._set_mode_vertical()
+            else:
+                self._set_mode_horizontal()
+            self.mark_dirty()
 
     def update_geometry(self, t):
         for s, tr, X in zip(self.series, THICK_RATES, Xs):
